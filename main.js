7ae6b5f61e214ed9262b7d56bc344ac44fd87156
IronOlympus <85447860+IronOlympus@users.noreply.github.com>
2022-05-25 12:26:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var leftwristy;
+var rightwristy;
 function setup(){
     canvas = createCanvas(350, 325);
     canvas.position(465,250);
@@ -5,8 +7,32 @@ function setup(){
     video = createCapture(VIDEO);
     video.position(465,250);
     video.size(350,325);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose' , gotPoses);
 }
 function preload(){
     harrypottersong = loadSound('music.mp3');
     peterpansong = loadSound('music2.mp3');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        leftwristy = results[0].pose.leftWrist.y;
+        rightwristy = results[0].pose.rightWrist.y;
+        console.log("Left wrist Y : " + leftwristy + " Right wrist Y : " + rightwristy);
+        check();
+    }
+}
+function modelLoaded(){
+    console.log("Model Initialized");
+}
+function check(){
+    if(rightwristy > 320){
+        harrypottersong.play();
+        peterpansong.stop();
+    }
+    if(leftwristy > 320){
+        peterpansong.play();
+        harrypottersong.stop();
+    }
 }
